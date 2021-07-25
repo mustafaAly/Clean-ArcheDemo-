c259ad1aff9b102154a60804b548904dfdf29157
mustafa aly <mustafaa.aly@outlook.com>
2021-07-25 19:27:01 +0200

    second commit

--- CleanArch/CleanArch.Mvc/Startup.cs
+++ CleanArch/CleanArch.Mvc/Startup.cs
@@ -1,3 +1,4 @@
+using CleanArch.Info.Data.Context;
 using CleanArch.Mvc.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -30,6 +31,9 @@ namespace CleanArch.Mvc
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
+            services.AddDbContext<UniversityDBContext>(options=>
+            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
