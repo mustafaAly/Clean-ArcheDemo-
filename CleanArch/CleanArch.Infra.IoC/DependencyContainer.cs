aa99db7a6c4445ac63f38b62de778fe0644a8d71
mustafa aly <mustafaa.aly@outlook.com>
2021-07-27 19:28:01 +0200

    lesson 10

---
+++ CleanArch/CleanArch.Infra.IoC/DependencyContainer.cs
@@ -0,0 +1,22 @@
+﻿using CleanArch.Application.Interfaces;
+using CleanArch.Application.Services;
+using CleanArch.Domain.Interfaces;
+using CleanArch.Info.Data.Repository;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArch.Infra.IoC
+{
+    class DependencyContainer
+    {
+        public static void RegisterService(IServiceCollection services)
+        {
+            services.AddScoped<ICourseService, CourseService>();
+
+
+            services.AddScoped<ICourseRepository, CourseRepository>();
+        }
+    }
+}
