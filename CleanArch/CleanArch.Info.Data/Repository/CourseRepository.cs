5ac471af8ec357128b963f2ee280ededc849b675
mustafa aly <mustafaa.aly@outlook.com>
2021-07-27 16:40:48 +0200

    ioc

---
+++ CleanArch/CleanArch.Info.Data/Repository/CourseRepository.cs
@@ -0,0 +1,24 @@
+﻿using CleanArch.Domain.Interfaces;
+using CleanArch.Domain.Models;
+using CleanArch.Info.Data.Context;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArch.Info.Data.Repository
+{
+
+    public class CourseRepository : ICourseRepository
+    {
+        private UniversityDBContext _ctx;
+        public CourseRepository(UniversityDBContext ctx)
+        {
+            _ctx = ctx;
+
+        }
+        public IEnumerable<Course> GetCourses()
+        {
+            return _ctx.Courses;
+        }
+    }
+}
