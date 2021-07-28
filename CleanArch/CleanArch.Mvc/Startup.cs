f5f1a30c6394fb4968af9e83eb6bb0e2444039e6
mustafa aly <mustafaa.aly@outlook.com>
2021-07-28 11:44:54 +0200

    initital commint for CQRs

--- CleanArch/CleanArch.Mvc/Startup.cs
+++ CleanArch/CleanArch.Mvc/Startup.cs
@@ -39,7 +39,10 @@ namespace CleanArch.Mvc
             services.AddControllersWithViews();
             services.AddRazorPages();
         }
+        //public static RegisterServices(this IServiceCollection services)
+        //{
 
+        //}
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
